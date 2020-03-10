538e6fbbbfaf69235b4d4b1e00fc12da25089f7e
Jeffrey Macko <mackoj@users.noreply.github.com>
2020-03-10 14:53:42 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .library(name: "BitriseCURLAPI", targets: ["BitriseCURLAPI"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.1"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0"),
   ],
   targets: [
     .target(name: "BitriseCURLAPI"),
